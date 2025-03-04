55efb66ee7bf04d35a0c23b138f8f79fc32aafae
Harm <harmfrima8@gmail.com>
2025-03-04 14:41:33 +0100

    Mijn wijzigingen op mijn eigen branch

---
+++ sw/airborne/modules/mav_course_exercise/mav_course_exercise.h
@@ -0,0 +1,10 @@
+#ifndef MAV_COURSE_EXERCISE_H
+#define MAV_COURSE_EXERCISE_H
+
+// Initialisatie functie
+void mav_course_exercise_init(void);
+
+// Periodieke functie
+void mav_course_exercise_periodic(void);
+
+#endif /* MAV_COURSE_EXERCISE_H */
