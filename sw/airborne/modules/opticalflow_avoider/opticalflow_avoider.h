091e2dbcbd03ecc8c9225a0807852a32997fd374
jwaschrauwen <jschrauwen@tudelft.nl>
2025-03-25 17:01:46 +0100

    Implemented the ability to change the threshold from GCS, implemented sort of geofencing and made some steps on improving obstacle detection

--- sw/airborne/modules/opticalflow_avoider/opticalflow_avoider.h
+++ sw/airborne/modules/opticalflow_avoider/opticalflow_avoider.h
@@ -3,6 +3,9 @@
 
 // External variable declaration
 extern float obstacle_threshold;
+extern float caution_threshold;
+extern float danger_threshold;
+
 
 void opticalflow_avoider_init(void);
 void opticalflow_avoider_periodic(void);
