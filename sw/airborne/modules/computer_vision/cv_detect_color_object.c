8e27b11895696a453d5d9b72926467d74b8d9ab4
Gijs <glutjebee@gmail.com>
2025-03-27 13:59:45 +0100

    increased speed, removed debug lines

--- sw/airborne/modules/computer_vision/cv_detect_color_object.c
+++ sw/airborne/modules/computer_vision/cv_detect_color_object.c
@@ -19,7 +19,7 @@ float y_min = 90, y_max = 210;
 float u_min = 75, u_max = 115;
 float v_min = 69, v_max = 145;
 
-float min_black = 3;
+float min_black = 5;
 
 #define BLOCK_SIZE 5
 #define GRID_ROWS 104
@@ -42,7 +42,6 @@ struct row_white_count_t {
 static struct row_white_count_t global_result;
 
 static void filter_particles(uint8_t *image_data, int width, int height) {
-    printf("[DEBUG] Entered filter_particles\n");
     uint8_t *filtered_image = malloc(width * height * 3);
     memcpy(filtered_image, image_data, width * height * 3);
 
@@ -70,17 +69,13 @@ static void filter_particles(uint8_t *image_data, int width, int height) {
 
     memcpy(image_data, filtered_image, width * height * 3);
     free(filtered_image);
-    printf("[DEBUG] Exiting filter_particles\n");
 }
 
 static void *process_image(void *arg) {
-    printf("[DEBUG] SHIT SHIT SHIT process_image CALLED\n");
     struct image_t *img = (struct image_t *)arg;
     int width = img->w;
     int height = img->h;
     uint8_t *buf = img->buf;
-
-    printf("[DEBUG] Image dims: %d x %d\n", width, height);
 
     int new_width = width / BLOCK_SIZE;
     int new_height = height / BLOCK_SIZE;
@@ -92,7 +87,6 @@ static void *process_image(void *arg) {
     frame_counter++;
 
     uint8_t *binary = malloc(width * height);
-    printf("[DEBUG] Allocated binary buffer at %p\n", binary);
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
@@ -116,7 +110,6 @@ static void *process_image(void *arg) {
     }
 
     uint8_t *downscaled = malloc(new_width * new_height * 3);
-    printf("[DEBUG] Allocated downscaled buffer at %p\n", downscaled);
 
     for (int i = 0; i < new_height; i++) {
         for (int j = 0; j < new_width; j++) {
@@ -178,9 +171,8 @@ static void *process_image(void *arg) {
     pthread_mutex_unlock(&mutex);
 
     if (frame_counter % 20 == 0) {
-        printf("[DEBUG] Frame %d, YUV center sample: Y=%d U=%d V=%d\n", frame_counter, buf[width * height], buf[width * height + 1], buf[width * height + 3]);
     
-        printf("[DEBUG] Downscaled matrix:\n");
+        printf("Downscaled matrix:\n");
         for (int i = 0; i < new_height; i++) {
             for (int j = 0; j < new_width; j++) {
                 int idx = (i * new_width + j) * 3;
@@ -193,17 +185,13 @@ static void *process_image(void *arg) {
 
     free(binary);
     free(downscaled);
-    printf("[DEBUG] Exiting process_image\n");
     return NULL;
 }
 
 void color_object_detector_init(void) {
     pthread_mutex_init(&mutex, NULL);
-    printf("[DEBUG] INIT -- Before memset\n");
     memset(&global_result, 0, sizeof(global_result));
-    printf("[DEBUG] INIT -- Before cv_add_to_device\n");
     cv_add_to_device(&COLOR_OBJECT_DETECTOR_CAMERA1, process_image, 10, 0);
-    printf("[DEBUG] INIT -- After cv_add_to_device\n");
 }
 
 void color_object_detector_periodic(void) {
@@ -227,7 +215,7 @@ void color_object_detector_periodic(void) {
                                global_result.white_counts[mid2] > 1 &&
                                global_result.white_counts[mid3] > 1 &&
                                global_result.white_counts[mid4] > 1 &&
-                               global_result.white_counts[mid5] > 1);
+                               global_result.white_counts[mid5] > 1); 
 
         printf("[DEBUG] PERIODIC -- condition_met: %d\n", condition_met);
         AbiSendMsgHORIZON_DETECTION(ORANGE_AVOIDER_VISUAL_DETECTION_ID, condition_met);
